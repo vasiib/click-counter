8dae0a8633494a93938f0a017112aafe07ea7fbf
vasiib <skvaseem999@gmail.com>
2022-02-16 00:20:10 +0530

    Full counter App changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count,setCount] = useState(0);
+  const [error,setError] = useState(false);
+
   return (
     <div data-test="component-app">
-      Vaseem
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp;
+        <span data-test="count">{count}</span>
+      </h1>
+      <br/>
+      <span data-test="error-message" className={`error ${error ? '' : 'hidden'}`}>
+        Counter can't go less than zero
+      </span>
+      <br/>
+      <button 
+        data-test="increment-button"
+        onClick={()=>{
+          if(error) setError(false);
+          setCount(count+1); 
+        }}>
+        Increment
+      </button>
+      <br/>
+      <button 
+        data-test="decrement-button"
+        onClick={()=>{
+          if(count <=0){
+            setError(true)
+          }else{
+            setCount(count-1)
+          }
+        }
+        }>
+        Decrement
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -5,25 +5,90 @@ import App from './App';
 // configuring enzyme
 Enzyme.configure({ adapter : new EnzymeAdapter() });
 
+/**
+ * 
+ * @returns Factory function to create a shallow Wrapper for the App component
+ */
+const setup =()=> shallow(<App/>); 
+
+const findAttributeByName = (wrapper, attValue, attName) => wrapper.find(`[${attName}='${attValue}']`);
+
 test('renders without error',()=>{
-  const wrapper = shallow(<App/>);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  //const appComponent = wrapper.find("[data-test='component-app']");
+  const appComponent = findAttributeByName(wrapper, "component-app", "data-test");
   expect(appComponent.length).toBe(1);
 });
 
 test('renders increment button',()=>{
-  
+  const wrapper = setup();
+  const button = findAttributeByName(wrapper,"increment-button","data-test");
+  expect(button.length).toBe(1);
 });
 
 
 test('renders counter display',()=>{
-  
+  const wrapper = setup();
+  const counterDisplay = findAttributeByName(wrapper,"counter-display","data-test");
+  expect(counterDisplay.length).toBe(1);
 });
 
 test('counter display start at 0',()=>{
-  
+  const wrapper = setup();
+  const counterDisplay = findAttributeByName(wrapper,"count","data-test");
+  expect(counterDisplay.text()).toBe("0");//text() always return string
 });
 
 test('clicking button increments counter display',()=>{
+  const wrapper= setup();
+//order is very imp -->
+  //find the button
+  const button = findAttributeByName(wrapper,"increment-button","data-test");
+
+  //click the button
+  button.simulate('click');
+
+  //find the display and test that number has been incremented
+  const count = findAttributeByName(wrapper,"count","data-test").text();
+  expect(count).toBe("1");
+});
+
+test('clicking button decrements counter display',()=>{
+  const wrapper= setup();
+//order is very imp -->
+  //find the button
+  const button = findAttributeByName(wrapper,"decrement-button","data-test");
   
+  //click the button
+  button.simulate('click');
+
+  // console.log(wrapper.html())
+  //find the display and test that number has been incremented
+  const errorMessage = findAttributeByName(wrapper,"error-message","data-test");
+  const errorHasHiddenClass = errorMessage.hasClass("hidden");
+
+  expect(errorHasHiddenClass).toBe(false);
+});
+
+test('clicking button decrements counter display and then increment button',()=>{
+  const wrapper= setup();
+//order is very imp -->
+  //find the button
+  const decButton = findAttributeByName(wrapper,"decrement-button","data-test");
+  
+  //click the button
+  decButton.simulate('click');
+
+  console.log(wrapper.html())
+  //find the button
+  const incButton = findAttributeByName(wrapper,"increment-button","data-test");
+  
+  //click the button
+  incButton.simulate('click');
+  console.log(wrapper.html())
+  //find the display and test that number has been incremented
+  const errorMessage = findAttributeByName(wrapper,"error-message","data-test");
+  const errorHasHiddenClass = errorMessage.hasClass("hidden");
+
+  expect(errorHasHiddenClass).toBe(true);
 });
